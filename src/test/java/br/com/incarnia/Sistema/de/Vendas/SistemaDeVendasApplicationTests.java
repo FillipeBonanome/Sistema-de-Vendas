3e22e8eb1a4df6881bcde37b542b885bef4ddba2
Fillipe <incarnialol2@gmail.com>
2025-07-12 18:09:47 -0300

    Initial commit

---
+++ src/test/java/br/com/incarnia/Sistema/de/Vendas/SistemaDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.incarnia.Sistema.de.Vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
